22c05657a213fce7db8126edc008ebd63e851b4e
rere2133 <rere2133@users.noreply.github.com>
2020-10-19 11:31:08 +0800

    no found page

--- src/pages/NoFoundPage.js
+++ src/pages/NoFoundPage.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import {withRouter} from 'react-router-dom'
+import {Button} from  'react-bootstrap'
 
 function NotFoundPage(props) {
+    // 或使用useHistory勾子也可以達到同樣效果
+    // let history = useHistory()
   return (
     <>
       <h1>找不到頁面</h1>
+      <Button onClick={()=>{
+          props.history.push('/product')
+      }}>回產品頁</Button>
+      <Button 
+      className = "ml-2"
+      onClick={()=>{
+          props.history.goBack()
+      }}>回上一頁</Button>
     </>
   )
 }
 
-export default NotFoundPage
+export default withRouter(NotFoundPage)
